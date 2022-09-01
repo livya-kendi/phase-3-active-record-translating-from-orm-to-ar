28a584545c5e3011da947e70c4508a47effa20d0
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-09-01 19:48:04 +0300

    lab complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
-class Dog
-end
+require "pry"
+require "active_record"
+
+#Database
+    ActiveRecord::Base.establish_connection(
+        adapter: "sqlite3",
+        database: "db/dogs.sqlite"
+      )
+
+#table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+    SQL
+    
+    ActiveRecord::Base.connection.execute(sql)
+
+#Logging output terminal
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+#class inherits from ActiveRecord::Base
+    class Dog < ActiveRecord::Base
+    end
+
+# binding.pry
+# ""
+
